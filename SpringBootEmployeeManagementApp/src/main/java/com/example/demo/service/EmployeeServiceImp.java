0c401e33f625b323f692c7697b443455be9b818f
sheela miranda <sheelamiranda27@gmail.com>
2025-01-23 11:23:25 +0530

    First COmmit from local

---
+++ SpringBootEmployeeManagementApp/src/main/java/com/example/demo/service/EmployeeServiceImp.java
@@ -0,0 +1,7 @@
+package com.example.demo.service;
+
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+
+public class EmployeeServiceImp extends JpaRepositoriesAutoConfiguration {
+
+}
